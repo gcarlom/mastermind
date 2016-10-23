1094eb2ca457c666cf3ca3be0581fbf593a16d3c
gcarlom <gcarlom@yahoo.se>
2016-10-23 22:04:00 +0200

    Allow direct access to static resources.Put static resources (CSS files, pictures, js files) under /src/main/webapp/resources/path-to-file.ext and access them from JSP using <c:url value="/resources/path-to-file.ext" />

--- src/main/java/com/gcmassari/mastermind/springconfig/WebConfig.java
+++ src/main/java/com/gcmassari/mastermind/springconfig/WebConfig.java
@@ -3,13 +3,29 @@ package com.gcmassari.mastermind.springconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.gcmassari.mastermind.controller")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	// Equivalent for <mvc:resources/> tags
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	// TODO GC: to remove as it seems this isn't need to make static resource accessible
+	// Equivalent for <mvc:default-servlet-handler/> tag
+//	@Override
+//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//		configurer.enable();
+//	}
 
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
--- src/main/webapp/WEB-INF/view/home.jsp
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -1,7 +1,17 @@
+<%@ taglib uri="http://java.sun.com/jstl/core" prefix="c" %>
+
 <!DOCTYPE HTML>
 <html>
   <head>
     <title>Mastermind</title>
+    <link href="<c:url value="/resources/css/main.css" />" rel="stylesheet" type="text/css" />
+
+ <%-- we need c:url to add the context path into the URL
+  c:url correctly resolves "/resources/css/main.css" -> "http://hostname/mastermind/resources/css/main.css"
+  If we used: <link href="/resources/css/main.css" ..> the browser would issue a GET for  "http://hostname/resources/css/main.css
+  and would get an 404 (not found) as response
+ --%>
+
   </head>
   <body>
 	<h1>Hello, ${name} !</h1>
