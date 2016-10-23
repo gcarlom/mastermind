957f3c239d017445baa360f55288efc67de3b1ff
gcarlom <gcarlom@yahoo.se>
2016-10-23 22:38:53 +0200

    Clean up Spring config class

--- src/main/java/com/gcmassari/mastermind/springconfig/WebConfig.java
+++ src/main/java/com/gcmassari/mastermind/springconfig/WebConfig.java
@@ -3,7 +3,6 @@ package com.gcmassari.mastermind.springconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,18 +13,11 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages="com.gcmassari.mastermind.controller")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-	// Equivalent for <mvc:resources/> tags
+	// Allows direct access to static resources: equivalent for <mvc:resources/> tags
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
-
-	// TODO GC: to remove as it seems this isn't need to make static resource accessible
-	// Equivalent for <mvc:default-servlet-handler/> tag
-//	@Override
-//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-//		configurer.enable();
-//	}
 
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
