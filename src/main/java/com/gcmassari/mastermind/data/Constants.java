dae390f385db0c45a3b2029b48c25ea156cd54b8
gcarlom <gcarlom@yahoo.se>
2017-06-06 00:37:38 +0200

    Updated version to 0.3.1, TODO file

--- src/main/java/com/gcmassari/mastermind/data/Constants.java
+++ src/main/java/com/gcmassari/mastermind/data/Constants.java
@@ -3,8 +3,8 @@ package com.gcmassari.mastermind.data;
 public class Constants {
 
     // TODO GC: let build_version and date be set from (gradle) build
-    public static final String BUILD_VERSION = "0.3.0";
+    public static final String BUILD_VERSION = "0.3.1";
 
-    public static final String BUILD_DATE = "2017-06-05";
+    public static final String BUILD_DATE = "2017-06-06";
 
 }
